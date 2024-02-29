3bbb347ecab623538387c113c92387e26fea6b2d
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-02-29 13:32:16 -0300

    Add game state sync

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from 'react';
+
 function Game({
   stashes,
   setStashesEmptied,
@@ -8,20 +10,37 @@ function Game({
 }) {
   let cards = [];
 
-  function handle() {
-    setStashesEmptied((current) => current + 1);
+  useEffect(() => {
+    function handleStashesEmptied() {
+      setStashesEmptied(stashesEmptied + 1);
+      /*
+      if (stashesEmptied >= stashes) {
+        console.log(stashes === stashesEmptied);
+        setSuccess(true);
+        setPlaying(false);
+      }*/
+    }
 
+    for (let i = 0; i < stashes; i += 1) {
+      document
+        .getElementById(`card-${i}`)
+        .addEventListener('click', handleStashesEmptied);
+    }
+  }, [stashesEmptied, setStashesEmptied, setPlaying, setSuccess, stashes]);
+
+  useEffect(() => {
     if (stashesEmptied >= stashes) {
       console.log(stashes === stashesEmptied);
       setSuccess(true);
       setPlaying(false);
     }
-  }
+  }, [stashesEmptied, stashes, setPlaying, setSuccess]);
+  // function handle() {}
 
   for (let i = 0; i < stashes; i += 1) {
     const cardId = `card-${i}`;
     cards.push(
-      <button onClick={handle} id={cardId} key={i}>
+      <button id={cardId} key={i}>
         Game
       </button>,
     );
