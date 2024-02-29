6b284e54e1f65f5e90d3707ab0a9157f7aa0d085
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-02-29 16:48:31 -0300

    Fix clicks adding multiple event listeners, cleanup logic

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Game({
   stashes,
@@ -10,41 +10,40 @@ function Game({
 }) {
   let cards = [];
 
-  useEffect(() => {
-    function handleStashesEmptied() {
-      setStashesEmptied(stashesEmptied + 1);
-      /*
-      if (stashesEmptied >= stashes) {
-        console.log(stashes === stashesEmptied);
-        setSuccess(true);
-        setPlaying(false);
-      }*/
-    }
+  const [uniqueIDs, setUniqueIDs] = useState({});
+  const [sad, setSad] = useState(false);
 
-    for (let i = 0; i < stashes; i += 1) {
-      document
-        .getElementById(`card-${i}`)
-        .addEventListener('click', handleStashesEmptied);
+  // Callback function, click handler
+  function handleStashesEmptied(e) {
+    setStashesEmptied(stashesEmptied + 1);
+    const newID = uniqueIDs[e.target.id] ? uniqueIDs[e.target.id] + 1 : 1;
+    setUniqueIDs({ ...uniqueIDs, [e.target.id]: newID });
+    if (newID > 1) {
+      setSad(true);
     }
-  }, [stashesEmptied, setStashesEmptied, setPlaying, setSuccess, stashes]);
+  }
 
-  useEffect(() => {
-    if (stashesEmptied >= stashes) {
-      console.log(stashes === stashesEmptied);
-      setSuccess(true);
-      setPlaying(false);
-    }
-  }, [stashesEmptied, stashes, setPlaying, setSuccess]);
-  // function handle() {}
-
+  // Creates all card elements
   for (let i = 0; i < stashes; i += 1) {
     const cardId = `card-${i}`;
     cards.push(
-      <button id={cardId} key={i}>
-        Game
+      <button onClick={handleStashesEmptied} id={cardId} key={i}>
+        Card
       </button>,
     );
   }
+
+  // Handles checking for sad or success //
+  useEffect(() => {
+    if (sad) {
+      setSuccess(false);
+      setPlaying(false);
+    } else if (stashesEmptied >= stashes && !sad) {
+      setSuccess(true);
+      setPlaying(false);
+    }
+  }, [sad, setPlaying, stashes, setSuccess, stashesEmptied]);
+
   return (
     <>
       <h1>Found Spots: {stashesEmptied}</h1>
