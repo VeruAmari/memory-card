c09d387d557402183c8cd32fbfd9007ab6ab8e96
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-03-01 21:23:36 -0300

    Implement beta randomize function

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -16,27 +16,72 @@ function Game({
 
   // TODO: Randomize cards order.
 
+  const [randomized, setRandomized] = useState('');
+
+  /**
+   * I need to create a function that randomizes
+   * the order of backgrounds in a list.
+   *
+   */
+
+  function randomize() {
+    console.log('Running randomize.');
+    let next = [];
+    while (next.length < stashes) {
+      const chosenRandomNumber = Math.floor(Math.random() * 30) % stashes;
+      console.log('Chosen Random Number: ', chosenRandomNumber);
+      const newNum = chosenRandomNumber;
+      if (!next.includes(newNum)) {
+        next.push(newNum);
+      }
+    }
+    const num = {};
+    for (let i = 0; i < stashes; i++) {
+      num[`${i}`] = next.pop();
+    }
+
+    setRandomized({ ...num });
+  }
+
   // Callback function, click handler
   function handleStashesEmptied(e) {
-    const newID = uniqueIDs[e.target.id] ? uniqueIDs[e.target.id] + 1 : 1;
-    setUniqueIDs({ ...uniqueIDs, [e.target.id]: newID });
+    const pic = e.target.style['background-image'];
+    const newID = uniqueIDs[pic] ? uniqueIDs[pic] + 1 : 1;
+    setUniqueIDs({ ...uniqueIDs, [pic]: newID });
     if (newID > 1) {
       setSad(true);
     } else {
       setStashesEmptied(stashesEmptied + 1);
     }
+    randomize();
   }
 
+  useEffect(() => {
+    let isRandom = false;
+    if (!isRandom) {
+      randomize();
+    }
+    return () => {
+      isRandom = true;
+    };
+  }, []);
+
   // Creates all card elements
+
+  // TODO: Randomized returns undefined when reaching  this function
   for (let i = 0; i < stashes; i += 1) {
+    console.log('Randomized, index', i, ': ', randomized[i]);
+    const index = randomized[i];
+    console.log(imageData.photos[index]);
     const cardId = `card-${i}`;
     const divStyle = {
-      backgroundImage: `url(${imageData.photos[i].src.tiny})`,
+      // TODO: Must replace i with index once its fixed
+      backgroundImage: `url(${imageData.photos[i].src.medium})`,
     };
     cards.push(
       <div
         className="hidden stash"
-        onClick={handleStashesEmptied}
+        onMouseUp={handleStashesEmptied}
         id={cardId}
         key={i}
         style={divStyle}
@@ -57,8 +102,8 @@ function Game({
 
   return (
     <>
-      <h1>Found Spots: {stashesEmptied}</h1>
-      <div>{cards}</div>
+      <h1>Stashes Visited: {stashesEmptied}</h1>
+      <div className="cards container">{cards}</div>
     </>
   );
 }
