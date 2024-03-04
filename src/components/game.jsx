2e95725ab96b60be5a73832db0c1828a576034de
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-03-04 17:20:52 -0300

    Remove status redundancy, refactor code to reduce re-renders

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -9,56 +9,59 @@ function Game({
   stashesEmptied,
   imageData,
 }) {
-  let cards = [];
-
+  // const cards = [];
+  const [cards, setCards] = useState('');
   const [uniqueIDs, setUniqueIDs] = useState({});
-  const [sad, setSad] = useState(false);
 
   const [randomizedOrder, setRandomizedOrder] = useState('');
-  const [toBeRandomized, setToBeRandomized] = useState(false);
+  const [toBeRandomized, setToBeRandomized] = useState(true);
 
-  // Randomize function
-  function randomize() {
-    console.log('Running randomize.');
-    let next = [];
-    while (next.length < stashes) {
-      const chosenRandomNumber = Math.floor(Math.random() * 30) % stashes;
-      const newNum = chosenRandomNumber;
-      if (!next.includes(newNum)) {
-        next.push(newNum);
+  useEffect(() => {
+    // Randomize function
+    function randomize() {
+      console.log('Running randomize.');
+      let next = [];
+      while (next.length < stashes) {
+        const chosenRandomNumber = Math.floor(Math.random() * 30) % stashes;
+        const newNum = chosenRandomNumber;
+        if (!next.includes(newNum)) {
+          next.push(newNum);
+        }
       }
-    }
-    const num = {};
-    for (let i = 0; i < stashes; i++) {
-      num[`${i}`] = next.pop();
+      const num = {};
+      for (let i = 0; i < stashes; i++) {
+        num[`${i}`] = next.pop();
+      }
+
+      setRandomizedOrder({ ...num });
     }
 
-    setRandomizedOrder({ ...num });
-  }
+    // Callback function, click handler
 
-  // Callback function, click handler
-  function handleClick(e) {
-    const pic = e.target.style['background-image'];
-    const newID = uniqueIDs[pic] ? uniqueIDs[pic] + 1 : 1;
-    setUniqueIDs({ ...uniqueIDs, [pic]: newID });
-    if (newID > 1) {
-      setSad(true);
-    } else {
-      setStashesEmptied((current) => current + 1);
-      setMaxStashesEmptied((current) =>
-        current <= stashesEmptied ? current + 1 : current,
-      );
+    // Randomize cards order
+    if (toBeRandomized) {
+      randomize();
+      setToBeRandomized(false);
     }
-    // Triggers randomizing on-click
-    setToBeRandomized(false);
-  }
-  // Randomize cards order
-  if (!toBeRandomized) {
-    randomize();
-    setToBeRandomized(true);
-  }
-  // Creates all card elements
-  if (stashes) {
+
+    function handleClick(e) {
+      const pic = e.target.style['background-image'];
+      const newID = uniqueIDs[pic] ? uniqueIDs[pic] + 1 : 1;
+      setUniqueIDs({ ...uniqueIDs, [pic]: newID });
+      if (newID > 1) {
+        setStatus('sad');
+      } else {
+        setStashesEmptied((current) => current + 1);
+        setMaxStashesEmptied((current) =>
+          current <= stashesEmptied ? current + 1 : current,
+        );
+      }
+      // Triggers randomizing on-click
+      setToBeRandomized(true);
+    }
+
+    // Creates all card elements
+    const setupCardsArray = [];
     for (let i = 0; i < stashes; i += 1) {
       console.log('Creating Cards.');
       const index = randomizedOrder[i];
@@ -67,7 +70,7 @@ function Game({
       const divStyle = {
         backgroundImage: `url(${image.src.medium})`,
       };
-      cards.push(
+      setupCardsArray.push(
         <div key={'card-' + i} className="card">
           <div className="hidden stash hover-effects"></div>
           <div
@@ -79,22 +82,24 @@ function Game({
         </div>,
       );
     }
-  }
-
-  // Handles checking for sad or success //
+    setCards(setupCardsArray);
+  }, [
+    imageData.photos,
+    randomizedOrder,
+    stashes,
+    setMaxStashesEmptied,
+    setStashesEmptied,
+    setStatus,
+    stashesEmptied,
+    uniqueIDs,
+    toBeRandomized,
+  ]);
   useEffect(() => {
-    if (sad) {
-      setStatus('sad');
-    } else if (stashesEmptied >= stashes) {
+    if (stashesEmptied >= stashes) {
       setStatus('success');
     }
-  }, [sad, setStatus, stashes, stashesEmptied]);
-
-  return (
-    <>
-      <div className="cards container colorize-bg2">{cards}</div>
-    </>
-  );
+  });
+  return <div className="cards container colorize-bg2">{cards}</div>;
 }
 
 export { Game };
